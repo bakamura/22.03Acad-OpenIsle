8eb327bba07b8acc1243b17908c9d4bbee8198fb
Bakamura <ra00297288@pucsp.edu.br>
2022-04-28 17:16:06 -0300

    Platform baseline finished + Enemy development

---
+++ Assets/Scripts/Platform/PlatformSpring.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpring : MonoBehaviour {
+
+    [System.NonSerialized] public bool active;
+    private Collider _collider; // May change
+    private MeshRenderer _mesh;
+
+    [SerializeField] private float _strengh;
+
+    private void Awake() {
+        _collider = GetComponent<Collider>();
+        _mesh = GetComponent<MeshRenderer>();
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.transform.tag == "Player") collision.transform.GetComponent<Rigidbody>().AddForce(transform.forward * _strengh, ForceMode.VelocityChange);
+    }
+
+    public void Activate(bool activating) {
+        active = activating;
+        _collider.enabled = activating;
+        _mesh.enabled = activating;
+    }
+
+}
