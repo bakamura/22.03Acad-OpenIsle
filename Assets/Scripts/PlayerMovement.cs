8af7c53d3a4d821de57f224f25d0b0fd8b04820e
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2022-04-29 17:05:58 -0300

    dash improvements

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,12 +9,10 @@ public class PlayerMovement : MonoBehaviour {
     [Header("Movement")]
 
     [SerializeField] private float _movementSpeed = 5f;
-    [SerializeField] private float _maxAirVelocity = 300f; // ???
-    [SerializeField] private float _airVelocityDecrease = .25f; // ???
+    [SerializeField] private float _maxAirVelocity = 300f;
     [Tooltip("How Fast the object rotates to input")]
     [SerializeField] private float _turnSmoothTime = .1f;
     private float _turnSmoothVelc; // Used by SmoothDampAngle because a static function can't store states.
-    private float _currentDashSpeed; //
 
     [Header("Jump")]
 
@@ -23,16 +21,14 @@ public class PlayerMovement : MonoBehaviour {
     [SerializeField] private Vector3 _boxSize = Vector3.one;
     [SerializeField] private LayerMask groundLayer;
     private bool _isGrounded;
-    // (4) Unnecessary?
-    [SerializeField] private float _distance;
-    private bool _hit;
-    private RaycastHit _hitinfo;
-    private float _airVelocity;
 
 
     [Header("Dash")]
 
     [SerializeField] private float _dashSpeed = 1000f;
+    [SerializeField] private float _dashDuration = .3f;
+    private bool _isDashing = false;
+    private Vector3 _dashDirection;
 
     private void Awake() {
         if (Instance == null) Instance = this;
@@ -41,35 +37,39 @@ public class PlayerMovement : MonoBehaviour {
 
     void Update() {
         // Jump
-        _isGrounded = Physics.OverlapBox(transform.position + groundCheckPoint, _boxSize / 2, Quaternion.identity, groundLayer).Length > 0 ; //
+        _isGrounded = Physics.OverlapBox(transform.position + groundCheckPoint, _boxSize / 2, Quaternion.identity, groundLayer).Length > 0; //
 
         if (PlayerInputs.jumpKeyPressed && _isGrounded) {
             PlayerInputs.jumpKeyPressed = false;
             if (_isGrounded) PlayerData.rb.AddForce(transform.up * _jumpStrengh, ForceMode.Force); // Check forcemode
-
-            // Unused for now
-            //_airVelocity = _jumpStrengh;
         }
 
         // Dash
         if (PlayerInputs.dashKeyPressed) {
             PlayerInputs.dashKeyPressed = false;
-            _currentDashSpeed = _dashSpeed;
+            _isDashing = true;
+            _dashDirection = GroundMovment() / _movementSpeed * _dashSpeed;
+            Invoke("EndDash", _dashDuration);
         }
     }
 
+    void EndDash() {
+        _isDashing = false;
+    }
     private void FixedUpdate() {
         //GroundCheck();
-
-        Vector3 TotalMovment = new Vector3(GroundMovment().x, /*AirMovment() / _movementSpeed */ PlayerData.rb.velocity.y, GroundMovment().z); // GRAVITY TOO HEAVY
-        PlayerData.rb.velocity = TotalMovment;
-        _currentDashSpeed = 1; // Instead, make player lose control while dashing, it's simpler
+        float airVelocity = Mathf.Clamp(PlayerData.rb.velocity.y, -_maxAirVelocity, _maxAirVelocity);
+        if (!_isDashing) {
+            Vector3 TotalMovment = new Vector3(GroundMovment().x, airVelocity, GroundMovment().z); // GRAVITY TOO HEAVY
+            PlayerData.rb.velocity = TotalMovment;
+        }
+        else PlayerData.rb.velocity = new Vector3(_dashDirection.x, airVelocity, _dashDirection.z);
     }
 
     private Vector3 GroundMovment() {
         if (PlayerInputs.horizontalAxis != 0 || PlayerInputs.verticalAxis != 0) {
             Vector3 groundMovment = GetMovmentDirection(new Vector3(PlayerInputs.horizontalAxis, 0, PlayerInputs.verticalAxis)).normalized;
-            return groundMovment * _movementSpeed * _currentDashSpeed;
+            return groundMovment * _movementSpeed;
         }
         else return Vector3.zero;
     }
@@ -88,35 +88,13 @@ public class PlayerMovement : MonoBehaviour {
     //     Physics.BoxCast(groundCheckPoint, _boxSize, -transform.up, out _hitinfo, Quaternion.identity, _distance, groundLayer);
     //    if (_hit) {
     //        _isGrounded = true;
-    //        if (_airVelocity < 0) _airVelocity = 0;
     //    }
     //    else _isGrounded = false;
     //}
 
-    private float AirMovment() {
-        if (!_isGrounded) {
-            _airVelocity -= _jumpStrengh * _airVelocityDecrease;
-            Mathf.Clamp(_airVelocity, -_maxAirVelocity, _maxAirVelocity);
-        }
-        return _airVelocity;
-    }
-
     void OnDrawGizmos() {
-        //Check if there has been a hit yet
-        if (_hit) {
-            Gizmos.color = Color.green;
-            //Draw a Ray forward from GameObject toward the hit
-            Gizmos.DrawRay(groundCheckPoint, -transform.up * _hitinfo.distance);
-            //Draw a cube that extends to where the hit exists
-            Gizmos.DrawWireCube(transform.position + groundCheckPoint + -transform.up * _hitinfo.distance, _boxSize);
-        }
-        //If there hasn't been a hit yet, draw the ray at the maximum distance
-        else {
-            Gizmos.color = Color.red;
-            //Draw a Ray forward from GameObject toward the maximum distance
-            Gizmos.DrawRay(groundCheckPoint, -transform.up * _hitinfo.distance);
-            //Draw a cube at the maximum distance
-            Gizmos.DrawWireCube(transform.position + groundCheckPoint + -transform.up * _hitinfo.distance, _boxSize);
-        }
+        if (_isGrounded) Gizmos.color = Color.green;
+        else Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position + groundCheckPoint, _boxSize);
     }
 }
