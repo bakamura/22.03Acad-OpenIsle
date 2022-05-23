c53dfdb3e389aa352843d1a9b49f1e8262fb5eef
Bakamura <ra00297288@pucsp.edu.br>
2022-05-22 21:31:48 -0300

    puzzle light visual start

--- Assets/Scripts/Objects/PuzzleLightSource.cs
+++ Assets/Scripts/Objects/PuzzleLightSource.cs
@@ -7,6 +7,13 @@ public class PuzzleLightSource : MonoBehaviour {
 
     [SerializeField] private Vector3 rayDirection = Vector3.forward;
     [SerializeField] private LayerMask detecLayer;
+    [SerializeField] private GameObject rayPrefab;
+    private GameObject rayInstance;
+
+    private void Start() {
+        rayInstance = Instantiate(rayPrefab);
+        rayInstance.GetComponent<MeshRenderer>().enabled = false;
+    }
 
     private void FixedUpdate() {
         RaycastHit hit;
@@ -16,6 +23,9 @@ public class PuzzleLightSource : MonoBehaviour {
             for (int i = 0; i < mirrorComponent.lightHits.Length; i++) {
                 if (mirrorComponent.lightHits[i] == null) {
                     mirrorComponent.lightHits[i] = new LightHit(rayDirection);
+                    rayInstance.transform.position = mirrorComponent.transform.position - transform.position;
+                    rayInstance.transform.eulerAngles = new Vector3(0, Mathf.Atan2(rayDirection.x, rayDirection.z) * Mathf.Rad2Deg, 0); // Test
+                    rayInstance.GetComponent<MeshRenderer>().enabled = true; // Make Stop somehow
                     break;
                 }
                 if (mirrorComponent.lightHits[i].angleReceived == Mathf.Atan2(rayDirection.x, rayDirection.z) * Mathf.Rad2Deg) break;
