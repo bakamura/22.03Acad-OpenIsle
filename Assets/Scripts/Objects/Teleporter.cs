222c7fa204aa356580642c5ee5a8565c12b8c9a6
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2022-06-16 11:56:52 -0300

    Particles

--- Assets/Scripts/Objects/Teleporter.cs
+++ Assets/Scripts/Objects/Teleporter.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Teleporter : MonoBehaviour
 {
     [SerializeField] private Vector3 _pointToTeleport;
-    //[SerializeField, Range(0,360)] private float _lookAngle;
+    [SerializeField, Range(0, 360)] private float _lookAngle;
 
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")) other.transform.SetPositionAndRotation(transform.position + _pointToTeleport, Quaternion.identity/*Quaternion.Euler(other.transform.rotation.x, _lookAngle, other.transform.rotation.z)*/);        
@@ -14,7 +14,8 @@ public class Teleporter : MonoBehaviour
     private void OnDrawGizmosSelected() {
         Gizmos.color = Color.red;
         Gizmos.DrawSphere(transform.position + _pointToTeleport, .3f);
-        //Gizmos.color = Color.blue;
-        //Gizmos.DrawLine(transform.position + _pointToTeleport,  new Vector3(Mathf.Sin(_lookAngle), 0, Mathf.Cos(_lookAngle)));
+        Gizmos.color = Color.blue;
+        Vector3 pos = transform.position + _pointToTeleport;
+        Gizmos.DrawLine(pos, new Vector3(Mathf.Sin(_lookAngle) + pos.x, pos.y, Mathf.Cos(_lookAngle) + pos.z));
     }
 }
