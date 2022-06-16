b8baa04734b0de1f557342ca3946b9149fd3711c
Bakamura <ra00297288@pucsp.edu.br>
2022-06-16 10:51:31 -0300

    Puzzle Resetter

---
+++ Assets/Scripts/Objects/PuzzleReseter.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PuzzleReseter : MonoBehaviour {
+
+    [SerializeField] private PuzzleObject[] _objectPrefabs;
+    private GameObject[] _instances;
+
+    private void Start() {
+        PlayerTools.onActivateAmulet += ResetPuzzle;
+    }
+
+    // Resets puzzle elements positions
+    private void ResetPuzzle() {
+        if ((PlayerData.Instance.transform.position - transform.position).magnitude < 2f) {
+            foreach (GameObject obj in _instances) Destroy(obj);
+
+            for (int i = 0; i < _objectPrefabs.Length; i++) {
+                _instances[i] = Instantiate(_objectPrefabs[i].puzObject, _objectPrefabs[i].position, _objectPrefabs[i].rotation);
+                _instances[i].transform.localScale = _objectPrefabs[i].scale;
+                if(_instances[i].GetComponent<PuzzleLightMirror>() != null && _objectPrefabs[i].variation == 1) _instances[i].GetComponent<PuzzleLightMirror>().ChangeRotation();
+            }
+        }
+    }
+
+}
+
+[System.Serializable]
+public class PuzzleObject {
+
+    public GameObject puzObject;
+    public Vector3 position;
+    public Quaternion rotation;
+    public Vector3 scale;
+    public int variation;
+
+}
