ba9814eb7988ef32848c6f48138341cbbb0e4c33
Bakamura <ra00297288@pucsp.edu.br>
2022-07-01 00:08:38 -0300

    Animator Player

--- Assets/Scripts/Objects/PuzzleLightReceptor.cs
+++ Assets/Scripts/Objects/PuzzleLightReceptor.cs
@@ -6,22 +6,10 @@ using UnityEngine.Events;
 public class PuzzleLightReceptor : MonoBehaviour {
 
     [HideInInspector] public UnityAction onActivate;
-    //private Vector3 baseScale;
-
-    
-    //private void Start() {
-        // Debug
-        //baseScale = transform.localScale;
-        //onActivate += DebugMethod;
-    //}
 
     // Calls a method when is hit by a ray from 'PuzzleLightMirror'
     public void Activate() {
-        onActivate.Invoke();
+        onActivate?.Invoke();
+        this.enabled = false;
     }
-
-    // Debug
-    //private void DebugMethod() {
-        //transform.localScale = 2 * baseScale;
-    //}
 }
