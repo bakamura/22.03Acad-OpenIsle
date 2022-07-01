3519893509ba84c1f03cce5776456e1534b46048
João Vitor <mendesjvm@hotmail.com>
2022-06-30 21:41:28 -0300

    Almost done

--- Assets/Scripts/Objects/PuzzleReseter.cs
+++ Assets/Scripts/Objects/PuzzleReseter.cs
@@ -4,6 +4,7 @@ public class PuzzleReseter : MonoBehaviour {
 
     [SerializeField] private PuzzleObject[] _objectPrefabs;
     private GameObject[] _instances;
+    private bool _playerInArea;
 
     private void Start() {
         PlayerTools.onActivateAmulet += ResetPuzzle;
@@ -11,17 +12,25 @@ public class PuzzleReseter : MonoBehaviour {
 
     // Resets puzzle elements positions
     private void ResetPuzzle() {
-        if ((PlayerData.Instance.transform.position - transform.position).magnitude < 2f) {
-            foreach (GameObject obj in _instances) Destroy(obj);
+        if (/*(PlayerData.Instance.transform.position - transform.position).magnitude < 2f*/_playerInArea) {
+            //foreach (GameObject obj in _instances) Destroy(obj);
 
             for (int i = 0; i < _objectPrefabs.Length; i++) {
-                _instances[i] = Instantiate(_objectPrefabs[i].puzObject, _objectPrefabs[i].position, _objectPrefabs[i].rotation);
-                _instances[i].transform.localScale = _objectPrefabs[i].scale;
-                if(_instances[i].GetComponent<PuzzleLightMirror>() != null && _objectPrefabs[i].variation == 1) _instances[i].GetComponent<PuzzleLightMirror>().ChangeRotation();
+                _objectPrefabs[i].puzObject.transform.SetPositionAndRotation(_objectPrefabs[i].position, _objectPrefabs[i].rotation);
+                _objectPrefabs[i].puzObject.transform.localScale = _objectPrefabs[i].scale;
+                //_instances[i] = Instantiate(_objectPrefabs[i].puzObject, _objectPrefabs[i].position, _objectPrefabs[i].rotation);
+                //_instances[i].transform.localScale = _objectPrefabs[i].scale;
+                //if (_instances[i].GetComponent<PuzzleLightMirror>() != null && _objectPrefabs[i].variation == 1) _instances[i].GetComponent<PuzzleLightMirror>().ChangeRotation();
             }
         }
     }
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) _playerInArea = true;
+    }
 
+    private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Player")) _playerInArea = false;
+    }
 }
 
 [System.Serializable]
